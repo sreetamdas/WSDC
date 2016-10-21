404028b4eff659770e6631215dbf08565b1d10ad
Sreetam <sreetamdas@gmail.com>
2016-10-21 13:46:58 +0530

    Fix Toggle in Mobile

--- main.js
+++ main.js
@@ -5,4 +5,9 @@ $(document).ready(function(){
 	});
 
 	$('.ui.dropdown').dropdown();
+    $('#open').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 500);
+        //$("html, body").scrollTop(0); //For without animation
+    });
 });
+
